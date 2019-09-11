6fdebec4f34377fdc972fbde6ed015f91d23774f
Yu Hao Ye <rxye1991@gmail.com>
2019-09-11 17:21:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def line(array)
+	if array.size() == 0
+		puts "The line is currently empty."
+	else
+		new_array = []
+		i = 0
+		until i == array.size()
+			new_array.push("#{i+1}. #{array[i]} ")
+			i += 1
+		end
+		
+		puts "The line is currently: #{new_array.join()}"
+	end
+end
+
+def take_a_number(array, person)
+	array[array.size] = person
+	puts "Welcome, #{person}. You are number #{array.size} in line."
+end 
+
+def now_serving(array)
+	if array.size == 0
+		puts "There is nobody waiting to be served!"
+	else
+		puts "Currently serving #{array[0]}."
+		array.shift()
+	end
+end
